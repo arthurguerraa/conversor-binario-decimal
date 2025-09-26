de87c5cfc26ce640bab7dfc66ad6e4ab0af67ad9
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-09-26 20:23:40 -0300

    script funcionando

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
-const binario = document.querySelector("#binario");
+const binarioInput = document.querySelector("#binario");
 const btnConverter = document.querySelector("#converter");
-const resultado = document.querySelector("resultado");
+const resultado = document.querySelector("#resultado");
 
-function converteDecimal(){
-    
+function converteDecimal(numeroBinario){
+    const decimal = parseInt(numeroBinario, 2);
+    return decimal;
 }
+
+btnConverter.addEventListener('click', () => {
+    const valorBinario = binarioInput.value;
+    const resultadoDecimal = converteDecimal(valorBinario);
+    resultado.innerHTML = `O resultado é ${resultadoDecimal}`;
+});
 
