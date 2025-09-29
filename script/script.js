0decc674c64c5f320947a1500d72a0e0e105213b
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-09-29 20:37:21 -0300

    Ajustes no css e javascript

--- script/script.js
+++ script/script.js
@@ -1,15 +1,33 @@
-const binarioInput = document.querySelector("#binario");
+const binarioInput = document.querySelector("#numero");
+const baseAtual = document.querySelector("#base");
+const baseConversao = document.querySelector("#conversao");
 const btnConverter = document.querySelector("#converter");
 const resultado = document.querySelector("#resultado");
 
-function converteDecimal(numeroBinario){
+function converteBinarioParaDecimal(numeroBinario){
     const decimal = parseInt(numeroBinario, 2);
     return decimal;
 }
 
+function converteDecimal(numeroBase, numeroConversao){
+    const numeroConvertido = "";
+    if(numeroConversao.value === "binario"){
+         numeroConvertido = (numeroBase, 2);
+    }else if(numeroConversao === "hexadecimal"){
+        numeroConvertido = (numeroBase, 16);
+    }else if(numeroConversao === "octal"){
+        numeroConvertido === (numeroBase, 8);
+    }
+
+    return numeroConvertido;
+}
+
 btnConverter.addEventListener('click', () => {
-    const valorBinario = binarioInput.value;
-    const resultadoDecimal = converteDecimal(valorBinario);
-    resultado.innerHTML = `O resultado é ${resultadoDecimal}`;
+    const resultadoConversao = ""; 
+    if (base.value === "decimal"){
+        resultadoConversao = converteDecimal(base, conversao);
+        resultado.innerHTML = `O resultado é ${resultadoDecimal}`;
+    }
+    
 });
 
